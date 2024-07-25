28ff139345eef10094df19550989270cd7caac83
Ryan <tjddnfkdls0@naver.com>
2024-07-25 15:57:35 +0900

    firebase setting

--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
 # 애플리케이션의 진입점
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, HTTPException, Depends
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from firebase_admin import auth
+import app.credentials.firebase_config
 from app.database import init_db
 from app.routes import chat_routes
 import uvicorn
@@ -9,6 +12,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 app = FastAPI()
+security = HTTPBearer()
 
 origins = [
     "http://localhost",
@@ -28,6 +32,44 @@ app.add_middleware(
 @app.on_event("startup")
 def on_startup():
     init_db()
+
+
+# accessToken 검증 함수
+def verify_token(auth_token: str):
+    try:
+        decoded_token = auth.verify_id_token(auth_token)
+        return decoded_token
+    except Exception as e:
+        raise HTTPException(status_code=401, detail="Invalid ID token")
+
+# 구글 로그인 요청 endpoint
+@app.post("/verify-token")
+async def verify_token_endpoint(request: Request):
+    data = await request.json()
+    id_token = data.get('idToken')
+    print(data)
+    print(id_token)
+    if not id_token:
+        raise HTTPException(status_code=400, detail="ID token is required")
+    decoded_token = verify_token(id_token)
+    print(decoded_token)
+    return {"message": "Token is valid", "user_id": decoded_token["uid"]}
+
+# swagger에서 토큰 유효성 검사 test용 api
+@app.post("/verify-jwt")
+async def verify_jwt_endpoint(credentials: HTTPAuthorizationCredentials = Depends(security)):
+    token = credentials.credentials
+    decoded_token = verify_token(token)
+    return {"message": "Token is valid", "user_id": decoded_token["uid"]}
+
+# Bearer 토큰을 HTTP 헤더에서 자동으로 추출하여 인증
+@app.get("/secure-endpoint")
+async def secure_endpoint(credentials: HTTPAuthorizationCredentials = security):
+    token = credentials.credentials
+    decoded_token = verify_token(token)
+    user_id = decoded_token['uid']
+    return {"message": "Secure endpoint", "user_id": user_id}
+
     
 app.include_router(chat_routes.router, prefix="/chats", tags=["chats"])
     
