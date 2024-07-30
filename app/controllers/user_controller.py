72ab7b8ee959b743d65b907f051ea554849cb514
modaing <hongi_@naver.com>
2024-07-30 16:27:05 +0900

    채팅 요청 목록 조회 + 채팅 요청 승인 (채팅방 입장)

--- app/controllers/user_controller.py
+++ app/controllers/user_controller.py
@@ -2,7 +2,7 @@
 
 from fastapi import APIRouter, Depends, HTTPException, Request
 from sqlalchemy.orm import Session
-from app.services.user_service import get_user_list_data
+from app.services.user_service import get_request_user_list_data, get_user_list_data
 from app.database import SessionLocal
 
 router = APIRouter()
@@ -17,7 +17,14 @@ def get_db():
 @router.get("/partners")
 def get_user_list(db: Session = Depends(get_db)):
     user_data = get_user_list_data(db)
-    print('user 리스트 : ', user_data)
     if not user_data:
         raise HTTPException(status_code=404, detail="UserList not found")
+    return user_data
+
+@router.get("/requestPartners")
+def get_request_user_list(request: Request, db: Session = Depends(get_db)):
+    uid = request.state.user['uid']
+    user_data = get_request_user_list_data(db, uid)
+    if not user_data:
+        raise HTTPException(status_code=404, detail="Request UserList not found")
     return user_data
--- app/services/user_service.py
+++ app/services/user_service.py
@@ -1,7 +1,47 @@
 from fastapi import HTTPException
 from sqlalchemy.orm import Session
-from app.database.models import User
+from app.database.models import User, ChatRoom
 from app.database import SessionLocal
 
 def get_user_list_data(db: Session):
     return db.query(User).all()
+
+def get_request_user_list_data(db: Session, uid: str):
+    user = db.query(User).filter(User.userCode == uid).first()
+
+    if user is None:
+        print(f'User with userCode {uid} not found.')
+        return []
+    
+    print('요청받은 user의 userId', user.userId)
+
+    chat_rooms = db.query(ChatRoom).filter(
+        ChatRoom.partnerId == user.userId,
+        ChatRoom.joinStatus == 1
+    ).all()
+    
+    if not chat_rooms:
+        print(f'No chat rooms found for partnerId {user.userId}.')
+        return []
+    
+    user_ids = [chat_room.userId for chat_room in chat_rooms]
+    related_users = db.query(User).filter(User.userId.in_(user_ids)).all()
+
+    results = []
+    for chat_room in chat_rooms:
+        related_users = next((user for user in related_users if user.userId == chat_room.userId), None)
+        if related_users:
+            results.append({
+                'chatRoomId': chat_room.chatRoomId,
+                'chatRoomName': chat_room.chatName,
+                'chatRoomDescript': chat_room.chatRoomDescript,
+                'userId': related_users.userId,
+                'userName': related_users.userName,
+                'userCode': related_users.userCode,
+                'profileImages': related_users.profileImages,
+                'description': related_users.description,
+                'nation': related_users.nation,
+                'email': related_users.email
+            })
+
+    return results
