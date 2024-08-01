d0daacc6eb5121455b01393c9758a10a7cf8dd87
modaing <hongi_@naver.com>
2024-08-01 15:47:03 +0900

    api restful 정리

--- app/controllers/chat_controller.py
+++ app/controllers/chat_controller.py
@@ -1,31 +1,22 @@
-# 채팅 컨트롤러
-
 from fastapi import APIRouter, Depends, HTTPException, Request
 from sqlalchemy.orm import Session
 from app.services.chat_service import get_live_chat_data, create_chat_room, update_live_chat_status
-from app.database import SessionLocal
+from app.database import SessionLocal, get_db
 
 router = APIRouter()
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-@router.get("/liveChat/{chat_room_id}")
+@router.get("/{chatRoomId}")
 def get_live_chat(chat_room_id: int, db: Session = Depends(get_db)):
     chat_data = get_live_chat_data(db, chat_room_id)
     if chat_data is None:
         raise HTTPException(status_code=404, detail="Chat room not found")
     return chat_data
 
-@router.post("/liveChat")
+@router.post("/")
 def create_live_chat(request: Request, chat_room: dict, db: Session = Depends(get_db)):
     uid = request.state.user['uid']
     return create_chat_room(db, chat_room, uid)
 
-@router.put("/liveChat/{chat_room_id}")
+@router.put("/{chatRoomId}/vacancy")
 def update_live_chat(chat_room_id: int, db: Session = Depends(get_db)):
     return update_live_chat_status(db, chat_room_id)
--- app/controllers/user_controller.py
+++ app/controllers/user_controller.py
@@ -1,34 +1,9 @@
-# 회원 컨트롤러
-
 from fastapi import APIRouter, Depends, HTTPException, Request
 from sqlalchemy.orm import Session
-from app.services.user_service import get_request_user_list_data, get_user_list_data, add_user_profile_data
-from app.database import SessionLocal
+from app.database import SessionLocal, get_db
+from app.services.user_service import add_user_profile_data
 
 router = APIRouter()
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-@router.get("/partners")
-def get_user_list(db: Session = Depends(get_db)):
-    user_data = get_user_list_data(db)
-    if not user_data:
-        raise HTTPException(status_code=404, detail="UserList not found")
-    return user_data
-
-@router.get("/requestPartners")
-def get_request_user_list(request: Request, db: Session = Depends(get_db)):
-    uid = request.state.user['uid']
-    print('state   ', uid)
-    user_data = get_request_user_list_data(db, uid)
-    if not user_data:
-        raise HTTPException(status_code=404, detail="Request UserList not found")
-    return user_data
 
 @router.post("/setUserProfile")
 async def create_user_profile(request : Request, db: Session = Depends(get_db)):
