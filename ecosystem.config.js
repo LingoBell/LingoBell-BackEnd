65707adb849b50f0219c0f567d239012df961d20
michaelpark77 <teletype777@gmail.com>
2024-08-26 17:51:31 +0900

    fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,9 +2,9 @@ module.exports = {
   apps : [
     {
       name: "backend",  // FastAPI 애플리케이션 이름
-      script: "/home/ubuntu/lingobell_backend/source/myenv/bin/uvicorn",
-      args: "app.main:app --host 0.0.0.0 --port 8000",  // FastAPI 실행 인자
-      interpreter: "/home/ubuntu/lingobell_backend/source/myenv/bin/python3",
+      script: "/home/ubuntu/lingobell_backend/source/myenv/bin/python3",
+      args: "-m uvicorn app.main:app --host 0.0.0.0 --port 8000",  // FastAPI 실행 인자
+      // interpreter: "/home/ubuntu/lingobell_backend/source/myenv/bin/python3",
       env: {
         "PYTHONUNBUFFERED": "1",
         "PATH": "/home/ubuntu/lingobell_backend/source/myenv/bin:$PATH",
@@ -18,7 +18,7 @@ module.exports = {
       host : '13.124.160.18',
       ref  : 'origin/main',
       repo : 'https://github.com/LingoBell/LingoBell-BackEnd.git',
-      path: '/home/ubuntu/lingobell_backend',
+      path : '/home/ubuntu/lingobell_backend',
       key: "~/develop/lingobell-EC2.pem",
       'post-deploy' : 'source myenv/bin/activate && pip install -r requirements.txt && pm2 reload ecosystem.config.js --env production && pip list'
     }
