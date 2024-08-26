f35d4ccdaffac3df37cff1408b1e0b81ac830368
michaelpark77 <teletype777@gmail.com>
2024-08-26 17:49:46 +0900

    fix ecosyustem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,15 +1,6 @@
 module.exports = {
   apps : [
     {
-      name: "rtc_socket",  // Node.js 애플리케이션 이름
-      script: "./socket-server.js",
-      // args: "socket-server.js",  // 실행할 파일
-      interpreter: "node",
-      env: {
-        NODE_ENV: "production",
-      },
-    },
-    {
       name: "backend",  // FastAPI 애플리케이션 이름
       script: "/home/ubuntu/lingobell_backend/source/myenv/bin/uvicorn",
       args: "app.main:app --host 0.0.0.0 --port 8000",  // FastAPI 실행 인자
@@ -29,7 +20,7 @@ module.exports = {
       repo : 'https://github.com/LingoBell/LingoBell-BackEnd.git',
       path: '/home/ubuntu/lingobell_backend',
       key: "~/develop/lingobell-EC2.pem",
-      'post-deploy' : 'source myenv/bin/activate && pip install -r requirements.txt && pm2 reload ecosystem.config.js --env production'
+      'post-deploy' : 'source myenv/bin/activate && pip install -r requirements.txt && pm2 reload ecosystem.config.js --env production && pip list'
     }
   }
 };
